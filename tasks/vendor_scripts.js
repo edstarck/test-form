2de44db92c08c90c7b8ba42f25acb228c7cdda1b
edstarck <webeduard@yandex.ru>
2018-11-26 10:27:53 +0300

    fix conflict

--- tasks/vendor_scripts.js
+++ tasks/vendor_scripts.js
@@ -1,21 +1,20 @@
-"use strict";
+'use strict';
 
-var gulp     = require('gulp');
-var concat   = require('gulp-concat');
-var uglify   = require('gulp-uglify');
-var gulpif   = require('gulp-if');
+var gulp = require('gulp');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var gulpif = require('gulp-if');
 
 // Запуск production NODE_ENV=production gulp script. По умолчанию development
-var isDevelopment = !process.env.NODE_ENV || process.env.NODE_ENV == 'development';
+var isDevelopment =
+  !process.env.NODE_ENV || process.env.NODE_ENV == 'development';
 
 module.exports = function(options) {
   return function() {
-    return gulp.src(options.src)
-    .pipe(concat('libs.js'))
-    .pipe(gulpif(!isDevelopment, uglify()))
-    .pipe(gulp.dest(options.dst));
+    return gulp
+      .src(options.src)
+      .pipe(concat('libs.js'))
+      .pipe(uglify())
+      .pipe(gulp.dest(options.dst));
   };
-
 };
-
-
--- tasks/vendor_styles.js
+++ tasks/vendor_styles.js
@@ -1,19 +1,17 @@
-"use strict";
+'use strict';
 
-var gulp     = require('gulp');
-var concat   = require('gulp-concat');
-var rename   = require('gulp-rename');
-var csso   = require('gulp-csso');
+var gulp = require('gulp');
+var concat = require('gulp-concat');
+var rename = require('gulp-rename');
+var csso = require('gulp-csso');
 
 module.exports = function(options) {
   return function() {
-    return gulp.src(options.src)
-    .pipe(concat('vendor.css'))
-    .pipe(csso())
-    .pipe(rename({suffix: '.min', prefix : ''}))
-    .pipe(gulp.dest(options.dst));
+    return gulp
+      .src(options.src)
+      .pipe(concat('vendor.css'))
+      .pipe(csso())
+      .pipe(rename({ suffix: '.min', prefix: '' }))
+      .pipe(gulp.dest(options.dst));
   };
-
 };
-
-
